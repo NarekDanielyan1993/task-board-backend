a352129b8e2726d48cf65b6c7ba30c733ca46e54
NarekDanielyan1993 <danielyann729@gmail.com>
2023-12-12 14:22:44 +0400

    dsadsad

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import App from 'lib/app';
 import MongoDb from 'lib/dbConnect';
 import errorMiddleware from 'middleware/error';
-import { ENV_VARIABLES } from './constant';
 import routes from './route';
 
 const app = new App(MongoDb.getInstance());
@@ -10,4 +9,4 @@ app.initializeMiddlewares();
 app.initializeRoutes(routes);
 app.addMiddleware(errorMiddleware);
 
-app.init(ENV_VARIABLES.DATABASE_PORT);
+app.init(5000);
