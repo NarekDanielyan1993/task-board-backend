8ae32d5a11871c24bfe3533a252647e565185424
NarekDanielyan1993 <danielyann729@gmail.com>
2024-04-08 14:51:42 +0400

    dsadas

--- src/constant/middleware/index.ts
+++ src/constant/middleware/index.ts
@@ -5,6 +5,7 @@ export const allowedOrigins = [
     'http://localhost:5000',
     'http://localhost:3000',
     'https://task-board-frontend-one.vercel.app',
+    'https://task-board-frontend.onrender.com',
 ];
 
 export const corsOptions: CorsOptions = {
